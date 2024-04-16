2982623ca81633c035fbd4004d3d457e0f0ebef0
nhandt <nhandt@nexlesoft.com>
2024-04-16 14:15:48 +0700

    feat: orkes demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,140 @@
-import Image from "next/image";
+"use client";
+
+import { Box, Button, Grid, Paper, TextField } from "@mui/material";
+import { hostname } from "os";
+import { useState } from "react";
+
+const BASE_API_URL = "https://orkes-demo-be.vercel.app";
 
 export default function Home() {
+  const [token, setToken] = useState("");
+  const [name, setName] = useState("");
+  // const [version, setVersion] = useState("");
+  const [host, setHost] = useState("https://orkesdev.orkesconductor.com/");
+  const [isError, setIsError] = useState(false);
+  const [data, setData] = useState<any>(null);
+  console.debug("🚀 ~ Home ~ data:", data);
+
+  const fetchWorkflow = async () => {
+    const result = await fetch(
+      `${BASE_API_URL}/workflow/${name}?host=${host}`,
+      {
+        headers: {
+          "x-authorization": token,
+        },
+        method: "GET",
+      }
+    );
+
+    const data = await result.json();
+
+    if (data) {
+      setData(data);
+    }
+  };
+
+  const histories: { role: string; message: string }[] =
+    data?.variables?.history || [];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <Box
+      sx={{
+        p: 8,
+        display: "flex",
+        alignItems: "center",
+        justifyItems: "center",
+        flexDirection: "column",
+        gap: 4,
+      }}
+    >
+      <Paper
+        sx={{
+          p: 4,
+          display: "flex",
+          alignItems: "center",
+          justifyItems: "center",
+          width: "100%",
+        }}
+      >
+        <Grid container spacing={3}>
+          <Grid item>Orkes demo</Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              label="Workflow execution id"
+              placeholder="Workflow execution id"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              label="Host"
+              placeholder="Host"
+              value={host}
+              onChange={(event) => setHost(event.target.value)}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              variant="outlined"
+              label="Token"
+              placeholder="Token"
+              value={token}
+              onChange={(event) => setToken(event.target.value)}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              onClick={() => {
+                fetchWorkflow();
+              }}
+            >
+              OK
+            </Button>
+          </Grid>
+        </Grid>
+      </Paper>
+      <Paper
+        sx={{
+          p: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyItems: "center",
+          width: "100%",
+        }}
+      >
+        <Box mb={3} sx={{ fontSize: 30, fontWeight: 600 }}>
+          History
+        </Box>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            background: "#ffffee",
+            // boxShadow: "2px 2px 2px 2px rgba(10,10,10,0.2)",
+          }}
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          {histories.map((item) => {
+            return (
+              <>
+                <Grid item xs={2}>
+                  {item.role}
+                </Grid>
+                <Grid item xs={10}>
+                  {item.message}
+                </Grid>
+              </>
+            );
+          })}
+        </Grid>
+      </Paper>
+    </Box>
   );
 }
